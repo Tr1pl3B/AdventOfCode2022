08f8cc57ce66bb6634cc293c9e1a8b2f6279801c
Tr1pl3B <buchholz.benedikt@t-online.de>
2022-12-01 17:56:42 +0100

    :tada: done with Day 1 :)

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+def elfHighesCarrCals():
+    puzIn = open('puzInput.txt', 'r')
+    cals = 0
+    calPerElf = list()
+    for line in puzIn:
+        if line == '\n':
+            calPerElf.append(cals)
+            cals = 0
+        else:
+            cals += int(line)
+    calPerElf.sort()
+    return calPerElf[-1] + calPerElf[-2] + calPerElf[-3]
+
 if __name__ == '__main__':
+    print(elfHighesCarrCals())
