0146f37049ba80cbc0778cb8719f171a41c41843
Tr1pl3B <buchholz.benedikt@t-online.de>
2022-12-10 12:31:46 +0100

    :construction: Left and Up is in the working...

--- dayNine.py
+++ dayNine.py
@@ -24,8 +24,28 @@ def GetPosVisOnce() -> int:
                     grid.append(['.'] * len(grid[headPos[0]]))
                 headPos[0] += 1
                 grid, tailPos = TailMovement(grid, headPos, tailPos, oldHead)
+        if direction == 'L':
+            for index in range(count):
+                oldHead = [headPos[0], headPos[1]+1]
+                if 0 == headPos[1]:
+                    grid[headPos[0]].insert(0, '.')
+                    grid[headPos[0]].append('.')
+                tailPos[1] += 1
+                grid, tailPos = TailMovement(grid, headPos, tailPos, oldHead)
+        if direction == 'U':
+            for index in range(count):
+                oldHead = [headPos[0]+1, headPos[1]]
+                if 0 == headPos[0]:
+                    grid.insert(0, ['.'] * len(grid[headPos[0]]))
+                    grid.append(['.'] * len(grid[headPos[0]]))
+                tailPos[0] += 1
+                grid, tailPos = TailMovement(grid, headPos, tailPos, oldHead)
 
-    print()
+    countPlaces = 0
+    for row in grid:
+        for column in row:
+            countPlaces += 1
+    return countPlaces
 
 def TailMovement(grid, headPos, tailPos, oldHead) -> list(list()):
     if headPos[0] - tailPos[0] + headPos[1] - tailPos[1] not in [-1, 0, 1]:
