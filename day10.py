32f5d348dc7055367bfbfb1b99f52a7b3ed8309a
Tr1pl3B <buchholz.benedikt@t-online.de>
2022-12-15 10:32:29 +0100

    :tada: day 10 part 1 done

---
+++ day10.py
@@ -0,0 +1,11 @@
+def GetSumOfSignalStrength() -> int:
+    signalStrength = 0
+    x = 1
+    for cycle, command in enumerate([command for line in open('inputDay10.txt', 'r') for command in line.split()]):
+        if cycle in [19, 59, 99, 139, 179, 219]:
+            print(cycle, x, (cycle+1)*x)
+            signalStrength = signalStrength + (cycle + 1) * x
+        if command.isnumeric() or command[0] == '-':
+            x += int(command)
+    return signalStrength
+
