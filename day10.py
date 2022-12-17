a310bba8a49a84ed83aebd94c5159b47b0b2f607
Tr1pl3B <buchholz.benedikt@t-online.de>
2022-12-18 00:02:13 +0100

    :recycle: removed print from working (first part) method, beginning with second part

--- day10.py
+++ day10.py
@@ -3,9 +3,14 @@ def GetSumOfSignalStrength() -> int:
     x = 1
     for cycle, command in enumerate([command for line in open('inputDay10.txt', 'r') for command in line.split()]):
         if cycle in [19, 59, 99, 139, 179, 219]:
-            print(cycle, x, (cycle+1)*x)
             signalStrength = signalStrength + (cycle + 1) * x
         if command.isnumeric() or command[0] == '-':
             x += int(command)
     return signalStrength
 
+def GetImmage() -> str:
+    x = 1
+    for cycle, command in enumerate([command for line in open('inputDay10.txt', 'r') for command in line.split()]):
+        if cycle in [19, 59, 99, 139, 179, 219]:
+            pass
+    return '-1'
